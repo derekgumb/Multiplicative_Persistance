11cdd87d00ee9d291f69ecb4ca2f77b5de527446
Derek Gumb <66969598+derekgumb@users.noreply.github.com>
2020-07-29 18:43:53 -0400

    Update Funct
    Cleaned up comments and var names.

--- Function.py
+++ Function.py
@@ -2,22 +2,22 @@ def mult_persistence(n):
     done_with_persistence = False
     accum = 0
 
+    #Finds number of digits in n:
     def find_int_length(my_int):
-        #Find number of digits in num
         return len(str(my_int))
 
+    #Finds product of digits in a n multiplied together:
     def mult(my_int):
-        #Funct that finds product of digits in a num multiplied together
         res = 1
         for i in range(len(str(my_int))):
             res = res * int(str(my_int)[i])
         return res
 
     while not done_with_persistence:
-        #Success if product of mult persistence is a 1-digit number
+        #Conclusion of program if n is a 1-digit number:
         if find_int_length(n) == 1:
             done_with_persistence = True
-        #Continue mult persistence if product has 2-digits or more
+        #Continue mult persistence if n has 2-digits or more
         elif find_int_length(n) >= 2:
             if find_int_length(mult(n))==1:
                 accum+=1
@@ -29,4 +29,4 @@ def mult_persistence(n):
 
     return accum
             
-print(mult_persistence(999))
+print(mult_persistence(25))
