9b339aeb224bc42566bdb7d60239e6cb5b23b471
Derek Gumb <66969598+derekgumb@users.noreply.github.com>
2020-07-30 12:01:20 -0400

    Update Function.py
    Streamlined and greatly shortened code.
    
    - Replaced find_int_length function with n=str(n) and len(n)
    - Replaced mult function with a for loop that iterates through str(n) and returns multiplicative persistence (p*=int(i))

--- Function.py
+++ Function.py
@@ -1,32 +1,15 @@
 def mult_persistence(n):
-    done_with_persistence = False
-    accum = 0
+    count = 0
+    n = str(n)
 
-    #Finds number of digits in n:
-    def find_int_length(my_int):
-        return len(str(my_int))
+    while len(n) > 1:
+        p = 1
+        for i in n:
+            p *= int(i)
+        n = str(p)
+        count += 1
 
-    #Finds product of digits in n multiplied together:
-    def mult(my_int):
-        res = 1
-        for i in range(len(str(my_int))):
-            res = res * int(str(my_int)[i])
-        return res
+    return count
 
-    while not done_with_persistence:
-        #Conclusion of program if n is a 1-digit number:
-        if find_int_length(n) == 1:
-            done_with_persistence = True
-        #Continue mult persistence if n has 2-digits or more:
-        elif find_int_length(n) >= 2:
-            if find_int_length(mult(n))==1:
-                accum+=1
-                done_with_persistence = True
-            elif find_int_length(mult(n))>=2:
-                accum+=1
-                n = mult(n)
-                continue
 
-    return accum
-            
 print(mult_persistence(999))
